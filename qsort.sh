a6d4b82723b691fd1f4a10eb9e5ee9e216764518
Yuxing Zhang <yzhang@cs.toronto.edu>
2024-05-31 03:11:37 +0800

    Fixed a bug caused by `i` not being local. Added randomness to the test example.

--- qsort.sh
+++ qsort.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
 qs() {
     if (($1 >= $2)); then return; fi
+    local i
     ((i = $1 + 1, j = $2))
     while true; do
-        while ((i <= $2)) && ((s[i] <= s[$1])); do ((i += 1)); done
-        while ((j > $1)) && ((s[j] >= s[$1])); do ((j -= 1)); done
+        while ((i <= $2 && s[i] <= s[$1])); do ((i += 1)); done
+        while ((j > $1 && s[j] >= s[$1])); do ((j -= 1)); done
         if ((i > j)); then break; fi
         ((t = s[i], s[i] = s[j], s[j] = t))
     done;
@@ -12,6 +14,8 @@ qs() {
     qs $i $2 
 }
 
-s=(18 67 48 68 9 54 85 31 67 37)
-qs 0 9
+s=()
+i=0; while ((i < 10)); do s+=($(($RANDOM % 100))); ((i++)); done;
+echo ${s[@]}
+qs 0 $((${#s[@]} - 1))
 echo ${s[@]}
