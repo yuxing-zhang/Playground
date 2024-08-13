ef9d05de6334c0e090f82c891dd91f082f00f1a3
Yuxing Zhang <yzhang@cs.toronto.edu>
2024-08-13 18:43:58 -0400

    Reorganized files to focus this repo on algorithms and data structures.

--- qsort.hs
+++
@@ -1,7 +0,0 @@
-qs :: (Ord a) => [a] -> [a]
-qs [] = []
-qs (x:xs) = qs [i | i <- xs, i < x] ++ [i | i <- xs, i == x] ++ [x] ++
-            qs [i | i <- xs, i > x]
-
-main :: IO ()
-main = print (qs [1, 3, 2, 0])
